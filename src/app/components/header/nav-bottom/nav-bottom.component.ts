334a5c770d0f9cd8817c85878eb55d9811e7cb3e
Francois Szczepkowski <francois.szcz@hotmail.fr>
2024-03-01 20:47:32 +0100

    ADD: Fichiers de configuration initiaux et structure de projet

---
+++ src/app/components/header/nav-bottom/nav-bottom.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-nav-bottom',
+  standalone: true,
+  imports: [],
+  templateUrl: './nav-bottom.component.html',
+  styleUrl: './nav-bottom.component.scss'
+})
+export class NavBottomComponent {
+
+}
---
+++ src/app/components/header/nav-top/nav-top.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-nav-top',
+  standalone: true,
+  imports: [],
+  templateUrl: './nav-top.component.html',
+  styleUrl: './nav-top.component.scss'
+})
+export class NavTopComponent {
+
+}
