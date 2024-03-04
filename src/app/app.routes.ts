30aa58747de7ab250bcb1e9bd7ac2459a8cd0c23
Francois Szczepkowski <francois.szcz@hotmail.fr>
2024-03-04 17:47:10 +0100

    UPDATE : page de connection mobil et pc

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,4 @@
 import { Routes } from '@angular/router';
+import { AuthComponent } from './components/auth/auth.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [{ path: 'login', component: AuthComponent }];
--- src/app/components/auth/auth.component.ts
+++ src/app/components/auth/auth.component.ts
@@ -1,12 +1,47 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-auth',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './auth.component.html',
-  styleUrl: './auth.component.scss'
+  styleUrl: './auth.component.scss',
 })
 export class AuthComponent {
+  isFormVisible = false; // Cette variable contrôle l'affichage du formulaire
+  annees: number[] = [];
+  mois: string[] = [
+    'Janvier',
+    'Février',
+    'Mars',
+    'Avril',
+    'Mai',
+    'Juin',
+    'Juillet',
+    'Août',
+    'Septembre',
+    'Octobre',
+    'Novembre',
+    'Décembre',
+  ];
+  jours: number[] = [];
 
+  constructor() {
+    this.genererAnnees();
+    this.genererJours();
+  }
+
+  genererAnnees() {
+    const anneeActuelle = new Date().getFullYear();
+    const anneeDebut = 1900; // Modifier selon le besoin
+    for (let annee = anneeActuelle; annee >= anneeDebut; annee--) {
+      this.annees.push(annee);
+    }
+  }
+  genererJours() {
+    for (let jour = 1; jour <= 31; jour++) {
+      this.jours.push(jour);
+    }
+  }
 }
